c774e39eb19280f52e5ba2b7cb6d1bf17eb52274
Chris_Li4 <Chris_Li4@Dell.com>
2021-04-06 06:34:15 -0700

    feat: wk1 homework

---
+++ WK1-twoSum.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+
+    for i, n := range(nums) {
+        _, prs := m[n]
+        if prs {
+            return []int{m[n], i}
+        }
+        m[target - n] = i 
+    }
+
+    return nil
+}
